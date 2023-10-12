8bb38d1c82b2b34a303c29ec1c238eb0ee139ef3
grisplata <miguegrisaled@gmail.com>
2023-10-11 23:35:51 -0500

    cgge9

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 //import App from './App';
-import FirstApp from './components/FirstApp';
+//import FirstApp from './components/Category';
+import { Category } from './components/Category';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <FirstApp value ={0}/>
+    <Category/>
   </React.StrictMode>
 );
 
