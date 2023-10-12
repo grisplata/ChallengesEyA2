c23ab7d8e83293297bdda9e12a8d33b5d05dc8e9
grisplata <miguegrisaled@gmail.com>
2023-10-11 19:21:31 -0500

    cgge7

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+const App = () => {
+  return(
+    <>
+      <h1>¡Hola Mundo!</h1>
+      <p>Bienvenido a React</p>
+    </>
+  )
+}
+
+export default App
---
+++ my-app/src/components/FirstApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function FirstApp() {
+    return (
+        <div>
+            <h1>Enfocado en React</h1>
+            <span>10</span>
+        </div>
+    )
+}
+
+export default FirstApp
